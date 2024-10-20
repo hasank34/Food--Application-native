12fdb7c48e192f38f44fc0840bdd4818e60ff66c
hasank34 <hasan3846fb@gmail.com>
2024-10-20 14:34:54 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Navigation from "./navigation";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+};
+
+export default App;
